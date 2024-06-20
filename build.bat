71d58d69bf49d019f7e6dad673ac7237630a6121
vieuxtemps <170285695+vieuxtemps@users.noreply.github.com>
2024-06-20 02:24:37 -0300

    updated build script

--- build.bat
+++ build.bat
@@ -85,6 +85,12 @@ cmd /c "echo %HASHES%" | clip
 
 echo:
 
+set /p choice="Delete ./build? (y/n): "
+if /i "!choice!"=="y" (
+  rmdir /S /Q build 2>nul
+)
+echo:
+
 set /p choice="Open explorer and GitHub? (y/n): "
 
 set "BROWSER=C:\Progra~1\Google\Chrome\Application\chrome.exe"
